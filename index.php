ea11fa50895daee4c9dd977d4949365313927432
NicolasDelille <nicolas.delille@gmail.com>
2015-07-17 10:51:16 +0200

    Add contents and some headers

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <!doctype html>
-<html lang="en">
+<html lang="fr">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<title>Pizza</title>
 </head>
 <body>
-	
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia, adipisci, iste, corrupti, sit saepe ipsum alias ipsam similique consequuntur ullam totam repellat molestias voluptatibus quae veritatis unde suscipit animi illum!</p>
 </body>
 </html>
