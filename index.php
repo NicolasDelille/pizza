ecb11003efdb34f1262ed874a2b4179f859e55e3
NicolasDelille <nicolas.delille@gmail.com>
2015-07-17 11:12:26 +0200

    CSS addings and Html addings

--- index.php
+++ index.php
@@ -3,8 +3,11 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Pizza</title>
+	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+
 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia, adipisci, iste, corrupti, sit saepe ipsum alias ipsam similique consequuntur ullam totam repellat molestias voluptatibus quae veritatis unde suscipit animi illum!</p>
+	
 </body>
 </html>
