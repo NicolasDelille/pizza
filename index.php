224dbc1e7d61aed6c1f55951456a9e90a26a6b9b
NicolasDelille <NicolasDelille@users.noreply.github.com>
2015-07-17 12:23:44 +0200

    change link js location

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
 	<meta charset="UTF-8">
 	<title>Pizza</title>
 	<link rel="stylesheet" href="css/style.css">
-	<script type="text/javascript" src="js/js.js"></script>
+	
 </head>
 <body>
 
 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia, adipisci, iste, corrupti, sit saepe ipsum alias ipsam similique consequuntur ullam totam repellat molestias voluptatibus quae veritatis unde suscipit animi illum!</p>
-	
+	<script type="text/javascript" src="js/js.js"></script>
 </body>
 </html>
